cee98b161a957d7865ee8f9e6ef11cda7bd56331
Sasha Maksyutenko <sasha.maksyutenko@gmail.com>
2025-01-03 16:59:33 +0200

    Adding Dark Mode in SideBar section

--- frontEnd/src/section/chat/Sidebar.jsx
+++ frontEnd/src/section/chat/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { Chat, SignOut } from '@phosphor-icons/react'
+import DarkModeSwitcher from '../../components/DarkModeSwitcher'
 export default function Sidebar () {
   return (
     <div className='flex flex-col border-r border-stroke p-2 dark:border-strokedark'>
@@ -6,7 +7,8 @@ export default function Sidebar () {
         <Chat size={24} />
       </div>
       <div className='flex flex-col grow'></div>
-      <div className=''>
+      <div className='space-y-4.5'>
+        <DarkModeSwitcher />
         <div className='mx-auto border rounded-md border-stroke p-2 dark:border-strokedark hover:bg-stone-100 hover:cursor-pointer'>
           <SignOut size={24} />
         </div>
