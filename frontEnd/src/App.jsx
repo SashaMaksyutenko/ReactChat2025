009e9df2bb8f104fbf83bb8a7177084755948249
Sasha Maksyutenko <sasha.maksyutenko@gmail.com>
2025-01-07 20:10:28 +0200

    Adding Verification page

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import Messages from './pages/Messages'
 import Login from './pages/auth/Login'
 import Signup from './pages/auth/Signup'
+import Verification from './pages/auth/Verification'
 
 export default function App () {
   return (
@@ -9,6 +10,7 @@ export default function App () {
       <Route index={true} element={<Messages />} />
       <Route path='/auth/login' element={<Login/>}/>
       <Route path='/auth/signup' element={<Signup/>}/>
+      <Route path='/auth/verify' element={<Verification/>}/>
     </Routes>
   )
 }
