8dee2c690d1f87e5af98848daf6d59e6297ec8c3
Sasha Maksyutenko <sasha.maksyutenko@gmail.com>
2025-01-11 16:16:48 +0200

    Adding logic for rendering images in media messages

--- frontEnd/src/components/Messages/Media.jsx
+++ frontEnd/src/components/Messages/Media.jsx
@@ -1,4 +1,5 @@
-import { Check, Checks } from "@phosphor-icons/react";
+import { Check, Checks } from '@phosphor-icons/react'
+import MediaMsgGrid from '../MediaMsgGrid'
 export default function Media ({
   incoming,
   author,
@@ -12,17 +13,19 @@ export default function Media ({
       <p className='mb-2.5 text-sm font-medium'>{author}</p>
       <div className='mb-2.5 rounded-2xl rounded-tl-none bg-gray px-5 py-3 dark:bg-boxdark-2 space-y-2'>
         {/* Media Msg Grid */}
+        <MediaMsgGrid incoming={incoming} />
         <p>{caption}</p>
       </div>
       <p className='text-xs'>{timestamp}</p>
     </div>
   ) : (
     <div className='max-w-125 ml-auto'>
-        <div className='mb-2.5 rounded-2xl rounded-br-none bg-primary px-5 py-3 space-y-2'>
-            {/* Media Msg Grid */}
-            <p className="text-white">{caption}</p>
-        </div>
-        <div className='flex flex-row items-center justify-end space-x-2'>
+      <div className='mb-2.5 rounded-2xl rounded-br-none bg-primary px-5 py-3 space-y-2'>
+        {/* Media Msg Grid */}
+        <MediaMsgGrid incoming={incoming} />
+        <p className='text-white'>{caption}</p>
+      </div>
+      <div className='flex flex-row items-center justify-end space-x-2'>
         <div
           className={`${
             read_receipt !== 'read'
