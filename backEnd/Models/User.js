044250773cf5dee9283a93e12c5bbb249bdad641
Sasha Maksyutenko <sasha.maksyutenko@gmail.com>
2025-01-17 23:14:00 +0200

    Building Auth APIs

--- backEnd/Models/User.js
+++ backEnd/Models/User.js
@@ -1,44 +1,78 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
-const userSchema=new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'Name is required'],
-    trim:true
-  },
-  email: {
-    type: String,
-    required: [true, 'Email is required'],
-    validate: {
-      validator: function (email) {
-        return validator.isEmail(email)
+const bcrypt = require('bcryptjs')
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Name is required'],
+      trim: true
+    },
+    email: {
+      type: String,
+      required: [true, 'Email is required'],
+      validate: {
+        validator: function (email) {
+          return validator.isEmail(email)
+        },
+        message: props => `Email (${props.value}) is invalid`
       },
-      message: props => `Email (${props.value}) is invalid`
+      unique: true
+    },
+    password: {
+      type: String
+    },
+    passwordChangedAt: {
+      type: Date
+    },
+    verified: {
+      type: Boolean,
+      default: false
+    },
+    otp: {
+      type: String
+    },
+    otp_expiry_time: {
+      type: Date
+    },
+    status: {
+      type: String,
+      enum: ['Online', 'Offline'],
+      default: 'Offline'
     }
   },
-  password: {
-    type: String
-  },
-  passwordChangedAt: {
-    type: Date
-  },
-  verified: {
-    type: Boolean,
-    default: false
-  },
-  otp: {
-    type: String
-  },
-  otp_expiry_time: {
-    type: Date
-  },
-  status: {
-    type: String,
-    enum:["Online","Offline"],
-    default:"Offline"
-  },
-},{
-    timestamps:true
+  {
+    timestamps: true
+  }
+)
+// PRE SAVE HOOK
+userSchema.pre('save', async function (next) {
+  // running this function if only otp modified
+  if (!this.isModified('otp') || this.otp) return next()
+  // Hash otp with cost of 12
+  this.otp = await bcrypt.hash(this.otp.toString(), 12)
+  console.log(this.otp.toString(), 'from PRE SAVE HOOK')
+  next()
 })
-const User=new mongoose.model("User", userSchema)
-module.exports=User
+// PRE SAVE HOOK
+userSchema.pre('save', async function (next) {
+  // running this function if only password modified
+  if (!this.isModified('password') || this.password) return next()
+  // Hash password with cost of 12
+  this.password = await bcrypt.hash(this.password.toString(), 12)
+  console.log(this.password.toString(), 'from PRE SAVE HOOK')
+  next()
+})
+// METHOD
+userSchema.methods.correctOTP = async function (candidateOTP, userOTP) {
+  return await bcrypt.compare(candidateOTP, userOTP)
+}
+// METHOD
+userSchema.methods.correctPassword = async function (
+  candidatePassword,
+  userPassword
+) {
+  return await bcrypt.compare(candidatePassword, userPassword)
+}
+const User = new mongoose.model('User', userSchema)
+module.exports = User
--- backEnd/controllers/authController.js
+++ backEnd/controllers/authController.js
@@ -1,9 +1,140 @@
+const User = require('../Models/User')
+const catchAsync = require('../utilities/catchAsync')
+const otpGenerator = require('otp-generator')
+const jwt = require('jsonwebtoken')
+// Sign JWT Token
+const signToken = userId => jwt.sign({ userId }, process.env.TOKEN_KEY)
 // register new user
-
-// send OTP 
-
-// verify OTP 
-
-// Login 
-
+exports.register = catchAsync(async (req, res, next) => {
+  const { name, email, password } = req.body
+  const existing_user = await User.findOne({
+    email: email
+  })
+  let new_user
+  if (existing_user && existing_user.verified === true) {
+    // email already in use
+    return res.status(400).json({
+      status: 'error',
+      message: 'Email already in use'
+    })
+  } else if (existing_user && existing_user.verified === false) {
+    // rewrite document and create new user
+    new_user = await User.findOneAndUpdate(
+      { email: email },
+      {
+        name,
+        password
+      },
+      {
+        new: true,
+        validateModifiedOnly: true
+      }
+    )
+  } else {
+    // if there is no previous record => create new record
+    new_user = await User.create({
+      name,
+      email,
+      password
+    })
+  }
+  req.userId = new_user._id
+  next()
+})
+// send OTP
+exports.sendOTP = catchAsync(async (req, res, next) => {
+  const { userId } = req
+  // generate new OTP
+  const new_otp = otpGenerator.generate(4, {
+    upperCaseAlphabets: false,
+    specialChars: false,
+    lowerCaseAlphabets: false
+  })
+  const otp_expiry_time = Date.now() + 10 * 60 * 1000 // 10 minutes after OTP is created
+  const user = await User.findByIdAndUpdate(
+    userId,
+    {
+      otp: new_otp.toString(),
+      otp_expiry_time: otp_expiry_time
+    },
+    { new: true, validateModifiedOnly: true }
+  )
+  // TODO => send otp via mail
+  res.status(200).json({
+    status: 'success',
+    message: 'OTP sent successfully'
+  })
+})
+// verify OTP
+exports.verifyOTP = catchAsync(async (req, res, next) => {
+  const { email, otp } = req.body
+  const user = await User.findOne({
+    email,
+    otp_expiry_time: { $gt: Date.now() }
+  })
+  if (!user) {
+    return res.status(400).json({
+      status: 'error',
+      message: 'Email is Invalid or OTP expired'
+    })
+  }
+  if (user.verified) {
+    return res.status(400).json({
+      status: 'error',
+      message: 'Email is already verified'
+    })
+  }
+  if (!(await user.correctOTP(otp, user.otp))) {
+    return res.status(400).json({
+      status: 'error',
+      message: 'OTP is incorrect'
+    })
+  }
+  // OTP is correct
+  user.verified = true
+  user.otp = undefined
+  await user.save({
+    new: true,
+    validateModifiedOnly: true
+  })
+  const token = signToken(user._id)
+  return res.status(200).json({
+    status: 'success',
+    message: 'OTP verified successfully',
+    token,
+    user_id: user._id
+  })
+})
+// Login
+exports.login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body
+  if (!email || !password) {
+    return res.status(400).json({
+      status: 'error',
+      message: 'Email and Password are required'
+    })
+  }
+  const user = await User.findOne({
+    email: email
+  }).select('+password')
+  if (!user || !user.password) {
+    return res.status(400).json({
+      status: 'error',
+      message: 'No record found for this email'
+    })
+  }
+  if (!user || !(await user.correctPassword(password, user.password))) {
+    return res.status(400).json({
+      status: 'error',
+      message: 'Email or Password is incorrect'
+    })
+  }
+  const token = signToken(user._id)
+  return res.status(200).json({
+    status: 'success',
+    message: 'Logged in successfully',
+    token,
+    user_id: user._id
+  })
+})
 // Protect
